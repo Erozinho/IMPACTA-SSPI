fed035d6aed117d25a2705bcb81f3cb7a6e4d094
Felipe Araujo <zenycsoficial@gmail.com>
2024-03-06 22:28:25 -0300

    🪡feat: Firebase connection

--- FAV/admin.py
+++ FAV/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from typing import Final
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
+
+
+# PASSO 2: CARREGAR O DISCORD TOKEN DE UM LUGAR SEGURO
+load_dotenv()
+KEY: Final[str] = os.getenv('KEY')
+
+
+
+cred = credentials.Certificate(KEY)
+firebase_admin.initialize_app(cred)
+
 
 # Register your models here.
