a9b14636b13262d21925c25a802e6baee48c7b52
Felipe Araujo <zenycsoficial@gmail.com>
2024-03-08 22:18:04 -0300

    AEPORRA FUNFO

--- impactasppi/settings.py
+++ impactasppi/settings.py
@@ -174,15 +174,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
