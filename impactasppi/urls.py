2556f951fabcb67f3ff02e2165b9c0d319424ea6
Felipe Araujo <zenycsoficial@gmail.com>
2024-03-14 20:13:57 -0300

    🐱‍🐉feat: login and register

--- impactasppi/urls.py
+++ impactasppi/urls.py
@@ -14,17 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.HomePageView.as_view(), name="home") ,
-    path("login/", views.LoginPageView.as_view(), name="login"),
+    path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path('postSignUp/', views.postSignUp),
+    path('logout/', views.logout, name="log"),
+    path('postLogin/', views.postLogin),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- impactasppi/views.py
+++ impactasppi/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView # Import TemplateView
+from django.shortcuts import redirect
 from django.contrib import messages
+import logging
 import pyrebase
+
+log = logging
 
 config = {'apiKey': "AIzaSyDTm56zPBOzgblJgsnUHD-qXI7-vXJVfk4",
   'authDomain': "impacta--sppi.firebaseapp.com",
@@ -14,18 +17,51 @@ config = {'apiKey': "AIzaSyDTm56zPBOzgblJgsnUHD-qXI7-vXJVfk4",
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-database = firebase.database() #comando necessário pra usar um banco de dados do firebase
+database = firebase.database()
 
 # Create your views here.
 
-class HomePageView(TemplateView):
-    template_name = "home.html"
+
+def login(request):
+    return render(request, "login.html")
 
 
-class LoginPageView(TemplateView):
-    template_name = "login.html"
-    redirect_authenticated_user = True
-    
-    def form_invalid(self, form):
-        messages.error(self.request,'E-mail ou senha inválidos!')
-        return self.render_to_response(self.get_context_data(form=form))
+def home(request):
+    return render(request, "home.html")
+
+
+def postLogin(request):
+    email = request.POST.get('lemail')
+    pasw = request.POST.get('lpass')
+    try:
+        user = auth.sign_in_with_email_and_password(email, pasw)
+    except user.DoesNotExist:
+        message = "Invalid Credentials!!Please ChecK your Data"
+        return render(request, "login.html", {"message": message})
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return redirect(home(), {"email": email})
+
+
+def postSignUp(request):
+    email = request.POST.get('email')
+    pasw = request.POST.get('pass')
+    name = request.POST.get('name')
+    print(name)
+    try:
+        user = auth.create_user_with_email_and_password(email, pasw)
+    except:
+        message = "e-mail já esta em uso!"
+        return redirect('login', {"message": message})
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return render(request, home(), {"email": email})
+
+
+def logout(request):
+    uid = request.session['uid']
+    try:
+        del request.session['uid']
+    except uid.DoesNotExist:
+        pass
+    return render(request, "login.html")
