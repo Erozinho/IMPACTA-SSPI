d19fdbf3a81fd98960c9d326cdbffcee3b22b262
Erozinho <zenycsoficial@gmail.com>
2024-03-17 19:31:59 -0300

    a

--- static/login.js
+++ static/login.js
@@ -1,12 +1,12 @@
 function openlogin(){
   document.getElementById('login-section').style.top = '12%';
-  document.getElementById('login-section').style.borderRadius = '100px';
+  document.getElementById('login-section').style.borderRadius = '213px';
   document.getElementById('singup').style.opacity = '0.6';
   
 }
 function closelogin(){
   document.getElementById('login-section').style.top = '90%';
-  document.getElementById('login-section').style.borderRadius = '250px';
+  document.getElementById('login-section').style.borderRadius = '290px';
   document.getElementById('singup').style.opacity = '1';
 
 }
