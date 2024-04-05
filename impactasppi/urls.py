68b57c6f21c86e0e1514fc1eebba42dc2e6bc3c1
Erozinho <zenycsoficial@gmail.com>
2024-04-05 20:38:27 -0300

    a

--- impactasppi/urls.py
+++ impactasppi/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path("register/", views.register, name="register"),  # type: ignore
     path('logout/', views.logout, name="log"),
     path('forget/', views.forget),  # type: ignore
-    path('terreno1/', views.terreno1, name="terreno1"),
-    path('terreno2/', views.terreno2, name="terreno2"),
-    path('terreno3/', views.terreno3, name="terreno3"),
+    path('terrenos/terreno1/', views.terreno1, name="terreno1"),
+    path('terrenos/terreno2/', views.terreno2, name="terreno2"),
+    path('terrenos/terreno3/', views.terreno3, name="terreno3"),
     path('terrenos/', views.terrenos, name="terrenos")
 ]
 
